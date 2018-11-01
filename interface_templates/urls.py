8a07223420bbf2ff328163ba3b821303d4979545
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-11-01 15:55:55 -0700

    first templates

--- interface_templates/urls.py
+++ interface_templates/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from templating import views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index),
+    url(r'^logged_in$', views.index_logged_in, name='logged_in'),
+    url(r'^about$', views.about, name='about')
 ]
--- templating/views.py
+++ templating/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    """
+    Starting page for app.
+    """
+    return render(request, 'templating/index.html')
+
+
+def index_logged_in(request):
+    """
+    Starting page for app.
+    """
+    return render(request, 'templating/index_logged_in.html')
+
+
+def about(request):
+    """
+    Starting page for app.
+    """
+    return render(request, 'templating/about.html')
